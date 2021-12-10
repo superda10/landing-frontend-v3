d38d4b58887f923315ac388b711b5b6f8d4118d0
Khắc Kiên <ii.khackien@gmail.com>
2021-12-10 16:11:31 +0700

    Init deps

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
+  important: true,
+  theme: {
+    extend: {
+      colors: {
+        light: {
+          10: '#FFF1',
+          20: '#FFF2',
+          30: '#FFF3',
+          40: '#FFF4',
+          50: '#FFF5',
+          60: '#FFF6',
+        },
+        dark: {
+          10: '#0001',
+          20: '#0002',
+          30: '#0003',
+          40: '#0004',
+          50: '#0005',
+          60: '#0006',
+        },
+        primary: {
+          main: '#CFB675',
+        },
+      },
+    },
+    flex: {
+      1: '1 1 0%',
+      2: '2 2 0%',
+      4: '3 3 0%',
+      3: '4 4 0%',
+    },
+  },
+  variants: {},
+  plugins: [],
+};
